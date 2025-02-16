d504076e2ba31e1a50e7d1404b39ad0d72f1062e
Rishav Goswami <goswamirishav520@gmail.com>
2025-02-17 02:18:42 +0530

    Add files via upload

---
+++ Pygame/Music Player/musicList.py
@@ -0,0 +1,6 @@
+#Music list (mp3)
+MAIN_MENU = 'assets/music/mainMenu.mp3'
+DIALOGUE = 'assets/music/dialogue.mp3'
+VIRUS = 'assets/music/virus.mp3'
+MICE = 'assets/music/mice.mp3'
+JOKER = 'assets/music/joker.mp3'
